98564bd8104ee60e8737055dc5e9db84007366a2
Ayush Yadav <sidharth197502@gmail.com>
2026-07-31 10:23:49 +0530

    starting to work on habit-logs

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HabitModule } from './habit/habit.module';
+import { HabitLogsModule } from './habit-logs/habit-logs.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, HabitModule],
+  imports: [AuthModule, PrismaModule, HabitModule, HabitLogsModule],
   controllers: [AppController],
   providers: [AppService],
 })
